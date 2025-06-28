4639f6f440802c78f5a45f94ca3051a39ffc6891
Ronaldo Ulisses <ulisses.22@hotmail.com>
2025-06-28 12:52:32 -0300

    Primeiro commit: projeto inicial

---
+++ analise_rfv.py
@@ -0,0 +1,8 @@
+
+# 1.BIBLIOTECAS VISUALIZAÇÃO E EXPLRAÇÃO DOS DADOS;
+import pandas as pd
+import numpy as np
+#import seaborn as sns
+#import matplotlib.pyplot as plt
+#from ydata_profiling import ProfileReport
+#import pandas_profiling
